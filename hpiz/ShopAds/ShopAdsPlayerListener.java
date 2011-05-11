fa15c2377d607600d1946ec7a16bcf383b5914ea
Chris Jacobsen <hpiz@live.com>
2011-05-10 20:57:36 -0400

    First Commit

---
+++ hpiz/ShopAds/ShopAdsPlayerListener.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hpiz.ShopAds;
+
+/**
+ *
+ * @author Chris
+ */
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+/**
+ * Handle events for all Player related events
+ * @author <yourname>
+ */
+public class ShopAdsPlayerListener extends PlayerListener {
+    private final ShopAds plugin;
+
+    public ShopAdsPlayerListener(ShopAds instance) {
+        plugin = instance;
+    }
+
+    //Insert Player related code he
+}
