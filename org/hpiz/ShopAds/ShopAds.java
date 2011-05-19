af6d40c816d5fe625382bf0d93bfe4aaaf74896f
Chris Jacobsen <hpiz@live.com>
2011-05-18 21:45:23 -0400

    Fixed User Properties and ShopTimes

--- org/hpiz/ShopAds/ShopAds.java
+++ org/hpiz/ShopAds/ShopAds.java
@@ -5,28 +5,22 @@ package org.hpiz.ShopAds;
  * @author Hpiz
  */
 import com.iConomy.*;
-import com.iConomy.system.Account;
 import com.iConomy.system.Holdings;
 
 import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginManager;
 import java.util.Properties;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
@@ -36,11 +30,6 @@ import java.util.Calendar;
 import java.util.Date;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.event.Event;
-import org.bukkit.event.Event.Priority;
-import org.bukkit.event.server.PluginDisableEvent;
-import org.bukkit.event.server.PluginEnableEvent;
-import org.bukkit.event.server.ServerListener;
 import org.bukkit.scheduler.BukkitScheduler;
 
 /**
@@ -216,6 +205,21 @@ public class ShopAds extends org.bukkit.plugin.java.JavaPlugin {
     public void writeUsers() {
         if (user.exists()) {
             try {
+                FileOutputStream in = new FileOutputStream(user);
+                ps.store(in, "");
+
+
+
+            } catch (IOException e) {
+            }
+        }
+        else{
+            try {
+                user.createNewFile();
+            } catch (IOException ex) {
+                Logger.getLogger(ShopAds.class.getName()).log(Level.SEVERE, null, ex);
+            }
+             try {
                 FileOutputStream in = new FileOutputStream(user);
                 ps.store(in, "");
 
@@ -445,7 +449,7 @@ public class ShopAds extends org.bukkit.plugin.java.JavaPlugin {
             }
             out.println("Name=" + action[0]);
           //  log.info("Printed: " + ("Name=" + action[0]));
-            out.println("Ends=" + (String.valueOf(dateNow.getTime()) + ((3600000.0) * (Long.parseLong(action[1])))));
+            out.println("Ends=" + (String.valueOf(dateNow.getTime()) + ((3600000) * (Long.parseLong(action[1])))));
             out.println("Location=" + (loc.getX() + "/" + loc.getY() + "/" + loc.getZ() + "," + loc.getPitch() + "," + loc.getYaw()));
             message = action[2];
             for (int i = 3; i < action.length; i++) {
