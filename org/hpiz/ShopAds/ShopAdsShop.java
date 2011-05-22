c0715f4ac25f69d5a856a851b4a775012a5849dc
Chris Jacobsen <hpiz@live.com>
2011-05-22 02:55:06 -0400

    -Added isExpired -Added isAdvertising for future implementation -Added ownerName for future implementation

--- org/hpiz/ShopAds/ShopAdsShop.java
+++ org/hpiz/ShopAds/ShopAdsShop.java
@@ -16,13 +16,20 @@ public class ShopAdsShop extends ShopAds{
     private String ad;
     private Double timeToEnd;
     private File shopFile;
+    private boolean isAdvertising;
+    private boolean isExpired;
+    private String shopOwner;
     
-    public ShopAdsShop (String n, String a, double[] l, Double t, File f){
-        name=n;
-        location =  l;
-        ad = a;
-        timeToEnd = t;
-        shopFile = f;        
+    public ShopAdsShop (String owner, String shopName, Double time, double[] locationOfPlayer, String advert, boolean advertising, boolean e, File f){
+        shopOwner=owner;
+        isAdvertising=advertising;
+        name=shopName;
+        location =  locationOfPlayer;
+        ad = advert;
+        timeToEnd = time;
+        shopFile = f;  
+        isExpired=e;
+        
     }
     public String getName(){
         return name;
@@ -42,8 +49,20 @@ public class ShopAdsShop extends ShopAds{
     public File getShopFile (){
         return shopFile;
     }
-    public void setName (String s){
-        name=s;
+    public boolean shopAdvertising(){
+        return isAdvertising;
+    }
+    public void setShopAdvertising (boolean b){
+        isAdvertising=b;
+    }
+    public boolean shopExpired (){
+        return isExpired;
+    }
+    public void setShopExpired (boolean b){
+        isExpired=b;
+    }
+    public String getShopOwner (){
+        return shopOwner;
     }
     
 }
